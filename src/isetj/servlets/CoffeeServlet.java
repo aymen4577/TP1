9a3431013257b95f32420d7ed4fc1868fd4cb681
aymen4577 <aymenmannai989@gmail.com>
2022-03-18 20:13:00 +0100

    first project

---
+++ src/isetj/servlets/CoffeeServlet.java
@@ -0,0 +1,43 @@
+package isetj.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class CoffeeServlet
+ */
+@WebServlet("/CoffeeServlet")
+public class CoffeeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CoffeeServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().println("<html><head><title>Servlet de coffee</title></head><body>"
+				+ " <img src=\"R.jpg\" height=\"250px\" width=\"250px\">"
+				+ "<p>Merci de nous avoir fait parvenir la remarque suivante concernant "
+				+ "<b>La qualité du service </b> Bonne</p></body></html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ src/isetj/servlets/SearchServlet.java
@@ -0,0 +1,51 @@
+package isetj.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SearchServlet
+ */
+@WebServlet("/SearchServlet")
+public class SearchServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SearchServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String T1 = request.getParameter("T1");
+		String Moteur = request.getParameter("Moteur");
+		
+		if (Moteur.equals("yahoo")) {
+			
+			response.sendRedirect("https://search.yahoo.com/search?p="+T1); 	
+		}
+		else {
+			
+			response.sendRedirect("https://www.google.com/search?q="+T1);  
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
